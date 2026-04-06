9084b89182e5b1dc0d68b96c38b8f4f1406f0edd
ivanko78 <balkan_boy_78@hotmail.com>
2026-04-06 22:55:08 +0200

    Update and rename combi.js to combi1.js

--- combi1.js
+++ combi1.js
@@ -1,20 +1,21 @@
+//cookie
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
-    d.setTime(d.getTime() (exdays*24*60*60*1000));
-    let expires = "expires=" d.toUTCString();
-    document.cookie = cname   "="   cvalue   ";"   expires   ";path=/";
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 function eraseCookie(cname) {
     const d = new Date();
-    d.setTime(d.getTime()   (24*60*60*1000));
-    let expires = "expires=" d.toUTCString();
-    document.cookie = cname   "=;"   expires   ";path=/";
+    d.setTime(d.getTime() + (24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=;" + expires + ";path=/";
 }
 function getCookie(cname) {
-    let name = cname   "=";
+    let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i  ) {
+    for(let i = 0; i <ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
